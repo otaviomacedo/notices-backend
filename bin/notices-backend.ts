58dcbd11fe458aec3df1cae909bf436667c37f8b
Otavio Macedo <otaviom@amazon.com>
2022-01-26 14:22:46 +0000

    Initial

--- bin/notices-backend.ts
+++ bin/notices-backend.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { NoticesBackendStack } from '../lib/notices-backend-stack';
+import 'source-map-support/register';
+import { PipelineStack } from '../lib/notices-backend-stack';
 
 const app = new cdk.App();
-new NoticesBackendStack(app, 'NoticesBackendStack', {
+new PipelineStack(app, 'NoticesBackend', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new NoticesBackendStack(app, 'NoticesBackendStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '280619947791', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/notices-backend-stack.ts
+++ lib/notices-backend-stack.ts
@@ -1,16 +1,57 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Stage, StageProps } from 'aws-cdk-lib';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as pipelines from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class NoticesBackendStack extends Stack {
+export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+      synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.gitHub('otaviomacedo/notices-backend', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NoticesBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    pipeline.addStage(new NoticesBackend(this, 'Prod', {
+      env: {
+        account: '280619947791',
+        region: 'us-east-1',
+      },
+    }));
+  }
+}
+
+class NoticesBackend extends Stage {
+  constructor(scope: Construct, id: string, props?: StageProps) {
+    super(scope, id, props);
+
+    new NoticesStack(this, 'NoticesStack');
+  }
+}
+
+export class NoticesStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const bucket = new s3.Bucket(this, 'DataSource');
+
+    const distribution = new cloudfront.Distribution(this, 'distribution', {
+      defaultBehavior: { origin: new origins.S3Origin(bucket) },
+    });
+
+    new s3deploy.BucketDeployment(this, 'deployment', {
+      destinationBucket: bucket,
+      distribution,
+      sources: [s3deploy.Source.asset('./data')],
+    });
   }
 }
