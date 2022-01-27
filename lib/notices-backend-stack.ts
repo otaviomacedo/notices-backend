55d1c28e29d7aa5cf67651951eaa053b1d08467a
Otavio Macedo <otaviom@amazon.com>
2022-01-27 11:49:15 +0000

    certificate

--- lib/notices-backend-stack.ts
+++ lib/notices-backend-stack.ts
@@ -1,9 +1,11 @@
 import { Stack, StackProps, Stage, StageProps } from 'aws-cdk-lib';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as pipelines from 'aws-cdk-lib/pipelines';
+import * as route53 from 'aws-cdk-lib/aws-route53';
 import { Construct } from 'constructs';
 
 export class PipelineStack extends Stack {
@@ -21,31 +23,53 @@ export class PipelineStack extends Stack {
       }),
     });
 
-    pipeline.addStage(new NoticesBackend(this, 'Prod', {
+    pipeline.addStage(new NoticesBackend(this, 'alpha', {
       env: {
         account: '280619947791',
         region: 'us-east-1',
       },
+      name: 'alpha'
     }));
   }
 }
 
+interface NoticesBackendProps extends StageProps {
+  name: string,
+}
+
 class NoticesBackend extends Stage {
-  constructor(scope: Construct, id: string, props?: StageProps) {
+  constructor(scope: Construct, id: string, props: NoticesBackendProps) {
     super(scope, id, props);
 
-    new NoticesStack(this, 'NoticesStack');
+    new NoticesStack(this, 'NoticesStack', {name: props.name});
   }
 }
 
+interface NoticesBackendStackProps extends StackProps {
+  name: string,
+}
+
 export class NoticesStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: NoticesBackendStackProps) {
     super(scope, id, props);
 
     const bucket = new s3.Bucket(this, 'DataSource');
 
+    const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'hostedZone', {
+      hostedZoneId: 'Z07404673IUTK24ZA0GMQ',
+      zoneName: 'cdk.dev-tools.aws.dev',
+    });
+
+    let domainName = `${props.name}.notices.cdk.dev-tools.aws.dev`;
+    const certificate = new acm.DnsValidatedCertificate(this, 'certificate', {
+      domainName,
+      hostedZone,
+    });
+
     const distribution = new cloudfront.Distribution(this, 'distribution', {
       defaultBehavior: { origin: new origins.S3Origin(bucket) },
+      certificate,
+      domainNames: [domainName],
     });
 
     new s3deploy.BucketDeployment(this, 'deployment', {
