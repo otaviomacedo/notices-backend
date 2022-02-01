3b756ccc19860b51569187e8e16b7d27af44609b
Otavio Macedo <otaviom@amazon.com>
2022-02-01 11:45:12 +0000

    issue exists

--- test/schema.test.ts
+++ test/schema.test.ts
@@ -1,6 +1,21 @@
+import { IncomingMessage } from 'http';
 import * as notices from '../data/notices.json';
 import { validateNotices } from '../lib/notice';
+const https = require('https');
 
 test('Notices are valid', () => {
   expect(() => validateNotices(notices.notices)).not.toThrow();
+});
+
+test('Issue exists', () => {
+  for (const notice of notices.notices) {
+    const url = `https://github.com/otaviomacedo/notices-backend/issues/${notice.issueNumber}`;
+    https.get(url, (res: IncomingMessage) => {
+      if (res.statusCode !== 200) {
+        fail(`Couldn't find issue ${url}`);
+      }
+    }).on('error', function(e: Error) {
+      fail(e);
+    });
+  }
 });
