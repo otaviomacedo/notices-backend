d99ad9ecc8ef1f45d57dbb3771c9131c4dd31761
Otavio Macedo <otaviom@amazon.com>
2022-02-01 10:26:38 +0000

    print token

--- .github/actions/sync-issues/src/hello.ts
+++ .github/actions/sync-issues/src/hello.ts
@@ -1,5 +1,9 @@
+// import * as github from '@actions/github';
+import * as core from '@actions/core';
+
 export function hello() {
-  console.log('Hello!!');
+  const githubToken = core.getInput('github_token', { required: true });
+  console.log(githubToken);
 }
 
 hello();
