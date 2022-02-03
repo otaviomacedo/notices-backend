edc3ccd2478f3cf7e5fbdf62e86605a52ebd9195
Otavio Macedo <otaviom@amazon.com>
2022-02-03 11:42:56 +0000

    synth pipeline stack

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -19,6 +19,7 @@ export class PipelineStack extends Stack {
           'npm ci',
           'npm run build',
           'npm test',
+          'npx cdk synth NoticesPipelineStack --verbose',
         ],
       }),
     });
